1175e36dc77e3d4fe0cd46b3747be75aab6f9ca5
Alissonsz <alisson_tecsouza@hotmail.com>
2019-06-22 23:44:55 -0300

    implements st part

--- ordener.cpp
+++ ordener.cpp
@@ -7,7 +7,7 @@ Ordener::Ordener(const char* fileName) : m_fileName (fileName) {
 		ReadInputFile();
 
 		while(!m_heap.empty()){
-			std::cout<<m_heap.top()<<std::endl;
+			std::cout<<m_heap.top().key<<" "<<m_heap.top().value<<std::endl;
 			m_heap.pop();
 		}	
 
@@ -22,20 +22,29 @@ Ordener::Ordener(const char* fileName) : m_fileName (fileName) {
 Ordener::~Ordener(){ }
 
 bool Ordener::ReadInputFile(){
-	Entry entry;
+	Reg aux;
 	int i=0;
 	while(true){
 		std::string readedKey = ReadStringFromInput();
 		std::string readedValue = ReadStringFromInput();
 
+		aux.key = readedKey;
+		aux.value = readedValue;
+		aux.marked = false;
+
 		if(readedKey == "FINISHED") break;
-		
 
-		m_heap.push(readedKey);
-		
+		if(m_someKeyAdded && aux.key < m_lastKeyAdded) aux.marked = true;
 
-		//std::cout<<readedFromIn<<std::endl;
+		m_heap.push(aux);
+		m_numberOfKeys++;
+		
+		if(m_heap.size() > MAX_HEAP){
+			CheckHeapAndWriteAtFile();
+		}
 	}
+
+	while(!m_heap.empty()) CheckHeapAndWriteAtFile();
 
 
 }
@@ -48,8 +57,6 @@ bool Ordener::OpenInputFile(){
 }
 
 std::string Ordener::ReadStringFromInput(){
-	//if(m_inFile.peek() == EOF) return std::string("EOF");
-
 	bool somethingToRead = false;
 	char currentChar;
 	std::string readedString;
@@ -63,7 +70,53 @@ std::string Ordener::ReadStringFromInput(){
 		
 	}
 	if(somethingToRead == false) return std::string("FINISHED");
-	//if(currentChar == ',') return std::string("ENDOFKEY");
+	
 	
 	return readedString;
+}
+
+void Ordener::CheckHeapAndWriteAtFile(){
+	if(m_currentRunFile.is_open() == false){
+		int fileToOpenId = CreateEmptyFile();
+
+		m_currentRunFile.open(std::string("run" + std::to_string(fileToOpenId) + ".dat").c_str(), std::fstream::in | std::fstream::out);
+	} else if(m_heap.top().marked == true) {
+		m_currentRunFile.close();
+		ResetMarkedElements();
+
+		int fileToOpenId = CreateEmptyFile();
+		m_currentRunFile.open(std::string("run" + std::to_string(fileToOpenId) + ".dat").c_str(), std::fstream::in | std::fstream::out);
+	}
+
+	Reg regToWrite = m_heap.top();
+	m_currentRunFile<<regToWrite.key<<","<<regToWrite.value+"\n";
+	m_lastKeyAdded = regToWrite.key;
+	m_someKeyAdded = true;
+
+	m_heap.pop();
+}
+
+void Ordener::ResetMarkedElements(){
+	std::vector<Reg> auxVector;
+
+	while(!m_heap.empty()){
+		Reg topReg = m_heap.top();
+		topReg.marked = false;
+		auxVector.push_back(topReg);
+		m_heap.pop();
+	}
+
+	for(int i = 0; i < auxVector.size(); i++) m_heap.push(auxVector[i]);
+}
+
+int Ordener::CreateEmptyFile(){
+	std::fstream newRunFile;
+	
+	int runId = 0;
+	if(m_runsIds.size() != 0) runId = m_runsIds[m_runsIds.size() - 1] + 1;
+	newRunFile.open(std::string("run" + std::to_string(runId) + ".dat").c_str(), std::fstream::out);
+	m_runsIds.push_back(runId);
+	newRunFile.close();
+
+	return runId;
 }
--- ordener.h
+++ ordener.h
@@ -6,16 +6,25 @@
 #include <string>
 #include <utility>
 #include <vector>
-#include <functional>
 
 #pragma pack(push, 1)
 struct Entry{
 	char key[20];
-	char value[50]; 
+	char value[50];
+
 };
 #pragma pack(pop)
 
+struct Reg{
+	std::string key;
+	std::string value;
+	bool marked;
 
+
+	bool operator < (const Reg& ro) const {
+		return (this->marked == true && ro.marked == false) || (this->marked == ro.marked && this->key > ro.key);
+	}
+};
 
 
 class Ordener{
@@ -28,14 +37,20 @@ private:
 	bool OpenInputFile();
 	bool ReadInputFile();
 	std::string ReadStringFromInput();
-	bool Compare(std::pair<std::string, bool> a, std::pair<std::string, bool> b) { 
-		return a.second == false && b.second == true || a.first < b.first;
-	}
+	int CreateEmptyFile();
+	void CheckHeapAndWriteAtFile();
+	void ResetMarkedElements();
 	
 
 	std::fstream m_inFile;
 	std::fstream m_outFile;
-	std::priority_queue <std::string, std::vector<std::string>> m_heap;
+	std::fstream m_currentRunFile;
+	std::priority_queue <Reg> m_heap;
+	std::vector<int> m_runsIds;
+	int m_numberOfRuns = 0;
+	int m_numberOfKeys = 0;
+	bool m_someKeyAdded = false;
+	std::string m_lastKeyAdded;
 
 
 	const int MAX_HEAP = 3;
